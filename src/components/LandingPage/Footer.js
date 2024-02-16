9ee29300ba8233d1857cbabd04adbca5f0fd1de3
jhon mondragon <jhonalmo23@gmail.com>
2024-02-16 17:38:01 -0500

    Se agregaron formularios a las opciones del footer y link a linkedin

--- src/components/LandingPage/Footer.js
+++ src/components/LandingPage/Footer.js
@@ -16,16 +16,17 @@ const FooterLanding = () => {
                 <Col span={8}>
                     <ul className='list-style-none'>
                         <h3>Contactanos</h3>
-                        <li className='border_select fs-15 fw-700 mg-bt-20'>Soporte tecnico</li>
-                        <li className='border_select fs-15 fw-700 mg-bt-20'>Ventas</li>
+                        <li className='fs-15 fw-700 mg-bt-20'><a className='txt-white' href='https://forms.gle/VnSreefcaaAixGnUA' target="_blank">Soporte Técnico</a></li>
+                        <li className='fs-15 fw-700 mg-bt-20'><a className='txt-white' href='https://www.linkedin.com/company/hiringpt' target="_blank">Linkedin</a></li>
+                        <li className='fs-15 fw-700 mg-bt-20'><a className='txt-white' href='https://forms.gle/KWJoSm9ZPC4sVGf5A' target="_blank">Ventas</a></li>
                     </ul>
                 </Col>
-        
+                
                 
                 <Col span={8}>
                     <ul className='list-style-none'>
                         <h3>Demostraciones</h3>
-                        <li className='border_select fs-15 fw-700 mg-bt-20'>Onboarding personalizado</li>
+                        <li className='fs-15 fw-700 mg-bt-20'><a className='txt-white' href='https://forms.gle/aVhQzCyFWBPQt6wq6' target="_blank">Onboarding personalizado</a></li>
                     </ul>
                 </Col>
         
@@ -33,8 +34,7 @@ const FooterLanding = () => {
                 <Col span={8}>
                     <ul className='list-style-none'>
                         <h3>Recursos</h3>
-                        <li className='border_select fs-15 fw-700 mg-bt-20'>Blog</li>
-                        <li className='border_select fs-15 fw-700 mg-bt-20'>Webinars</li>
+                        <li className='fs-15 fw-700 mg-bt-20'><a className='txt-white' href='https://whip-shield-807.notion.site/Webinars-5f43f8db11984854bdc5ce6fe76d9d68?pvs=4' target="_blank">Webinars</a></li>
                     </ul>
                 </Col>
             </Row>
